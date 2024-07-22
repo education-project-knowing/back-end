aa4148d99b521884ab25192a60e8bb4bb0ba9d83
conchohi <lar3242@gmail.com>
2024-07-22 13:12:50 +0900

    Initial commit

---
+++ src/main/java/education/knowing/KnowingApplication.java
@@ -0,0 +1,13 @@
+package education.knowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/education/knowing/KnowingApplicationTests.java
@@ -0,0 +1,13 @@
+package education.knowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
