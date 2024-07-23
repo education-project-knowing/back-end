956d345a4b0d2c779ebcf50fc5f5f035184091a8
conchohi <lar3242@gmail.com>
2024-07-23 22:19:23 +0900

    Feat: add login filter with jwt
    - Jwt 토큰 발행 클래스 생성
    - JSON 기반 로그인 필터 생성
    - 로그인 성공 시 access token, refresh token 발행

---
+++ src/main/java/education/knowing/filter/LoginFilter.java
@@ -0,0 +1,90 @@
+package education.knowing.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import education.knowing.dto.LoginDto;
+import education.knowing.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public LoginFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            LoginDto loginRequest = mapper.readValue(request.getInputStream(), LoginDto.class);
+            String username = loginRequest.getUsername();
+            String password = loginRequest.getPassword();
+
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username,password);
+
+            return authenticationManager.authenticate(token);
+        } catch (IOException e) {
+            throw new AuthenticationServiceException("인증 실패 : " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String username = authResult.getName();
+
+        Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        //access : 10분, refresh : 1일
+        String access = jwtUtil.createJwt("access",username, role,10*60*1000L);
+        String refresh = jwtUtil.createJwt("refresh",username, role,24*60*60*1000L);
+
+        //클라이언트 header 에 Access token 등록
+        response.setHeader("Authorization","Bearer " + access);
+
+        //cookie 에 Refresh token 등록
+        response.addCookie(createCookie("refresh", refresh));
+        response.setStatus(HttpStatus.OK.value());
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("{\"status\" : 401, \"message\" : \"인증되지 않음\"}");
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        //보일 위치 - 전역
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        //cookie.setSecure(true);
+
+        return cookie;
+    }
+}
---
+++ src/main/java/education/knowing/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package education.knowing.service;
+
+import education.knowing.entity.CustomUserDetails;
+import education.knowing.entity.User;
+import education.knowing.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("존재하지 않는 아이디"));
+
+        return new CustomUserDetails(user);
+    }
+
+}
