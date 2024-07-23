d6f0f7a696b904bc458e7ed7a0733c253d88225f
conchohi <lar3242@gmail.com>
2024-07-23 22:22:03 +0900

    Feat: add findByUsername in UserRepository
    - 아이디 기반 User Entity 찾는 메소드 생성

--- src/main/java/education/knowing/repository/UserRepository.java
+++ src/main/java/education/knowing/repository/UserRepository.java
@@ -3,8 +3,12 @@ package education.knowing.repository;
 import education.knowing.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
+
+    Optional<User> findByUsername(String username);
 }
