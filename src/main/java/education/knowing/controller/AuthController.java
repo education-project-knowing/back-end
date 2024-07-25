0d3297c40149cb1fd726c68cec6b8a2dc69bd7d7
conchohi <lar3242@gmail.com>
2024-07-25 15:13:58 +0900

    Feat: 이메일 인증, 검증 추가

--- src/main/java/education/knowing/controller/AuthController.java
+++ src/main/java/education/knowing/controller/AuthController.java
@@ -1,11 +1,12 @@
 package education.knowing.controller;
 
-import education.knowing.dto.UserDto;
+import education.knowing.dto.CertificationDto;
+import education.knowing.dto.request.SignUpRequestDto;
 import education.knowing.dto.response.ResponseDto;
-import education.knowing.exception.BusinessError;
-import education.knowing.exception.BusinessLogicException;
 import education.knowing.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,22 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
     @PostMapping("/sign-up")
-    public ResponseEntity<ResponseDto<Object>> join(@RequestBody UserDto userDto){
-        return ResponseEntity.ok(authService.join(userDto));
+    public ResponseEntity<ResponseDto<Object>> join(@RequestBody @Valid SignUpRequestDto userSignUpDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.join(userSignUpDto));
     }
 
     @PostMapping("/id-check")
-    public ResponseEntity<?> idCheck(@RequestBody UserDto userDto){
-        return ResponseEntity.ok(authService.idCheck(userDto.getUsername()));
+    public ResponseEntity<ResponseDto<Boolean>> checkId(@RequestBody SignUpRequestDto userSignUpDto){
+        return ResponseEntity.ok(new ResponseDto<>(200, "아이디 중복 검사", authService.idCheck(userSignUpDto.getUsername())));
     }
 
     @PostMapping("/email-check")
-    public ResponseEntity<?> emailCheck(@RequestBody UserDto userDto){
-        return ResponseEntity.ok(authService.emailCheck(userDto.getEmail()));
+    public ResponseEntity<ResponseDto<Boolean>> checkEmail(@RequestBody SignUpRequestDto userSignUpDto){
+        return ResponseEntity.ok(new ResponseDto<>(200, "비밀번호 중복 검사",authService.emailCheck(userSignUpDto.getEmail())));
     }
 
     @PostMapping("/nickname-check")
-    public ResponseEntity<?> nicknameCheck(@RequestBody UserDto userDto){
-        return ResponseEntity.ok(authService.nicknameCheck(userDto.getNickname()));
+    public ResponseEntity<ResponseDto<Boolean>> checkNickname(@RequestBody SignUpRequestDto userSignUpDto){
+        return ResponseEntity.ok(new ResponseDto<>(200, "닉네임 중복 검사",authService.nicknameCheck(userSignUpDto.getNickname())));
+    }
+
+    @PostMapping("/email/send")
+    public ResponseEntity<ResponseDto<CertificationDto>> sendEmail(@RequestBody CertificationDto certificationDto){
+        return ResponseEntity.ok(authService.sendCertificationEmail(certificationDto));
+    }
+
+    @PostMapping("/email/certification")
+    public ResponseEntity<ResponseDto<Object>> certificationEmail(@RequestBody CertificationDto certificationDto){
+        return ResponseEntity.ok(authService.certificationEmail(certificationDto));
     }
 }
