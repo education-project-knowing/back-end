fbf0ef018b6861d064d542dcb5d9a60eab39c9d8
conchohi <lar3242@gmail.com>
2024-07-26 14:23:14 +0900

    Add: 회원가입 관련 DTO 생성

---
+++ src/main/java/education/knowing/dto/response/SignUpResponseDto.java
@@ -0,0 +1,14 @@
+package education.knowing.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpResponseDto {
+    private Long userId;
+}
