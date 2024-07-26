a337b0bbfd41660bf2aeac545da41786f67909fa
conchohi <lar3242@gmail.com>
2024-07-26 16:09:07 +0900

    Add: 폴더 내 문답을 넣기 위한 중간 테이블 Repository 추가

---
+++ src/main/java/education/knowing/repository/FolderQnaRepository.java
@@ -0,0 +1,7 @@
+package education.knowing.repository;
+
+import education.knowing.entity.FolderQna;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderQnaRepository extends JpaRepository<FolderQna, Long> {
+}
