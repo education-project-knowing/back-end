0cae46633064bfc0b9dad1e5bb9a5f7c14304aaf
conchohi <lar3242@gmail.com>
2024-07-27 22:44:10 +0900

    Add: 비즈니스 로직 상 에러 추가

--- src/main/java/education/knowing/exception/BusinessError.java
+++ src/main/java/education/knowing/exception/BusinessError.java
@@ -8,16 +8,19 @@ import lombok.Getter;
 public enum BusinessError {
     //400 (잘못된 요청)
     CERTIFICATION_FAIL(400, "이메일 인증 실패"),
+    OPINION_SUBMIT_LIMITED_TO_10MINUTES(400, "10분 이내 사용자 의견 재작성 불가"),
 
     //403 (권한 부족)
     NOT_FOLDER_CREATOR(403, "해당 폴더의 작성자가 아님"),
     NOT_QUESTION_CREATOR(403, "해당 문답의 작성자가 아님"),
+    NOT_OPINION_CREATOR(403, "해당 사용자 의견의 작성자가 아님"),
 
     //404 (존재하지 않음)
     USER_NOT_FOUND(404, "존재하지 않는 유저"),
     EMAIL_NOT_FOUND(404, "존재하지 않는 이메일"),
     FOLDER_NOT_FOUND(404, "존재하지 않는 폴더"),
     QUESTION_NOT_FOUND(404, "존재하지 않는 문답"),
+    OPINION_NOT_FOUND(404, "존재하지 않는 사용자 의견"),
 
     //409 (충돌)
     DUPLICATED_ID(409, "이미 존재하는 아이디"),
