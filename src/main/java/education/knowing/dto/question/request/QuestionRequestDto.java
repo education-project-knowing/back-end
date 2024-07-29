9ea93fba106730ba07e7558ad7012c4f2b8201be
conchohi <lar3242@gmail.com>
2024-07-29 20:08:30 +0900

    Fix: 문답 추가 시 속성 매핑을 위한 어노테이션 추가

--- src/main/java/education/knowing/dto/question/request/QuestionRequestDto.java
+++ src/main/java/education/knowing/dto/question/request/QuestionRequestDto.java
@@ -1,11 +1,13 @@
 package education.knowing.dto.question.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class QuestionRequestDto {
+    @JsonProperty("fNo")
     private Long fNo;
     private String question;
     private String answer;
