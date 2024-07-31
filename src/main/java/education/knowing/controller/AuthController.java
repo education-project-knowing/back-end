13507d695e2bba540a0def9066c6b40c6db4414a
conchohi <lar3242@gmail.com>
2024-07-31 14:15:09 +0900

    Modify: Email 관련 로직 분리

--- src/main/java/education/knowing/controller/AuthController.java
+++ src/main/java/education/knowing/controller/AuthController.java
@@ -1,13 +1,11 @@
 package education.knowing.controller;
 
 import education.knowing.dto.auth.request.*;
-import education.knowing.dto.auth.response.CertificationResponseDto;
 import education.knowing.dto.auth.response.SignUpResponseDto;
 import education.knowing.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -37,36 +35,6 @@ public class AuthController {
     @ResponseStatus(HttpStatus.OK)
     public boolean checkNickname(@RequestBody @Valid NicknameCheckRequestDto nicknameCheckRequestDto){
         return authService.nicknameCheck(nicknameCheckRequestDto.getNickname());
-    }
-
-    @PostMapping("/email/send")
-    @ResponseStatus(HttpStatus.OK)
-    public void sendEmail(@RequestBody @Valid CertificationRequestDto certificationDto) {
-        certificationDto.setPurpose("회원가입 인증메일");
-        authService.sendCertificationEmail(certificationDto);
-    }
-    @PostMapping("/email/send/id")
-    @ResponseStatus(HttpStatus.OK)
-    public void sendEmailForId(@RequestBody @Valid CertificationRequestDto certificationDto){
-        certificationDto.setPurpose("아이디 찾기 인증메일");
-        authService.sendCertificationEmail(certificationDto);
-    }
-    @PostMapping("/email/send/password")
-    @ResponseStatus(HttpStatus.OK)
-    public void sendEmailForPassword(@RequestBody @Valid FindPasswordRequestDto findPasswordRequestDto){
-        findPasswordRequestDto.setPurpose("비밀번호 찾기 인증메일");
-        authService.sendCertificationEmailForPassword(findPasswordRequestDto);
-    }
-
-    @PostMapping("/email/certification")
-    @ResponseStatus(HttpStatus.OK)
-    public void certificationEmail(@RequestBody @Valid CertificationRequestDto certificationDto){
-        authService.certificationEmail(certificationDto);
-    }
-
-    @PostMapping("/email/certification/id")
-    public ResponseEntity<CertificationResponseDto> certificationEmailForId(@RequestBody @Valid CertificationRequestDto certificationDto){
-        return ResponseEntity.ok(authService.certificationEmailForId(certificationDto));
     }
 
 }
--- src/main/java/education/knowing/service/AuthService.java
+++ src/main/java/education/knowing/service/AuthService.java
@@ -1,26 +1,16 @@
 package education.knowing.service;
 
 import education.knowing.constant.Role;
-import education.knowing.dto.auth.request.CertificationRequestDto;
-import education.knowing.dto.auth.request.FindPasswordRequestDto;
 import education.knowing.dto.auth.request.SignUpRequestDto;
-import education.knowing.dto.ResponseDto;
-import education.knowing.dto.auth.response.CertificationResponseDto;
 import education.knowing.dto.auth.response.SignUpResponseDto;
-import education.knowing.entity.EmailCertification;
 import education.knowing.entity.User;
 import education.knowing.exception.BusinessError;
 import education.knowing.exception.BusinessLogicException;
-import education.knowing.repository.EmailCertificationRepository;
 import education.knowing.repository.UserRepository;
-import education.knowing.util.MailSendUtil;
-import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Random;
 
 @Service
 @Transactional
@@ -28,18 +18,16 @@ import java.util.Random;
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
-    private final EmailCertificationRepository certificationRepository;
-    private final MailSendUtil mailSendUtil;
 
-    public SignUpResponseDto join(SignUpRequestDto userDto){
+    public SignUpResponseDto join(SignUpRequestDto userDto) {
         //중복 체크
-        if(idCheck(userDto.getUsername())) {
+        if (idCheck(userDto.getUsername())) {
             throw new BusinessLogicException(BusinessError.DUPLICATED_ID);
         }
-        if(emailCheck(userDto.getEmail())){
+        if (emailCheck(userDto.getEmail())) {
             throw new BusinessLogicException(BusinessError.DUPLICATED_EMAIL);
         }
-        if(nicknameCheck(userDto.getNickname())){
+        if (nicknameCheck(userDto.getNickname())) {
             throw new BusinessLogicException(BusinessError.DUPLICATED_NICKNAME);
         }
 
@@ -57,78 +45,20 @@ public class AuthService {
     }
 
     @Transactional(readOnly = true)
-    public boolean idCheck(String id){
+    public boolean idCheck(String id) {
         return userRepository.existsByUsername(id);
     }
+
     @Transactional(readOnly = true)
-    public boolean emailCheck(String email){
+    public boolean emailCheck(String email) {
         return userRepository.existsByEmail(email);
 
     }
+
     @Transactional(readOnly = true)
     public boolean nicknameCheck(String nickname) {
         return userRepository.existsByNickname(nickname);
     }
 
-    public void sendCertificationEmail(CertificationRequestDto certificationDto){
-        //이메일이 존재하지 않으면
-        if(!emailCheck(certificationDto.getEmail())){
-            throw new BusinessLogicException(BusinessError.EMAIL_NOT_FOUND);
-        }
 
-        String email = certificationDto.getEmail();
-        String certificationNumber = createCertificationNumber();
-
-        EmailCertification emailCertification = EmailCertification.builder()
-                .email(email)
-                .certificationNumber(certificationNumber)
-                .build();
-
-        try {
-            mailSendUtil.sendCertificationMail(email, certificationDto.getPurpose(), certificationNumber);
-        } catch (MessagingException e) {
-            throw new BusinessLogicException(BusinessError.MAIL_FAIL);
-        }
-
-        certificationRepository.save(emailCertification);
-    }
-
-    public void sendCertificationEmailForPassword(FindPasswordRequestDto findPasswordRequestDto) {
-        if(userRepository.existsByUsernameAndEmail(findPasswordRequestDto.getId(), findPasswordRequestDto.getEmail())){
-            throw new BusinessLogicException(BusinessError.WRONG_ID_OR_EMAIL);
-        }
-        sendCertificationEmail(findPasswordRequestDto);
-    }
-
-    public void certificationEmail(CertificationRequestDto certificationDto){
-        if(!certificationRepository.existsByEmailAndCertificationNumber(
-                certificationDto.getEmail(),
-                certificationDto.getCertificationNumber())){
-            throw new BusinessLogicException(BusinessError.CERTIFICATION_FAIL);
-        }
-
-        certificationRepository.deleteById(certificationDto.getEmail());
-    }
-    public CertificationResponseDto certificationEmailForId(CertificationRequestDto certificationDto) {
-        if(!certificationRepository.existsByEmailAndCertificationNumber(
-                certificationDto.getEmail(),
-                certificationDto.getCertificationNumber())){
-            throw new BusinessLogicException(BusinessError.CERTIFICATION_FAIL);
-        }
-        User user = userRepository.findByEmail(certificationDto.getEmail());
-
-        certificationRepository.deleteById(certificationDto.getEmail());
-
-        return new CertificationResponseDto(user.getUsername());
-    }
-
-    private String createCertificationNumber(){
-        Random random = new Random();
-        String[] numbers = {"0","1","2","3","4","5","6","7","8","9"};
-        StringBuilder certificationNumber = new StringBuilder();
-        for(int i = 0; i < 6; i++){
-            certificationNumber.append(numbers[random.nextInt(10)]);
-        }
-        return certificationNumber.toString();
-    }
 }
